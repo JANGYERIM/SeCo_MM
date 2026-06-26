80600754f45da0a75a3f5167e7fa9de357fcded2
dpfla3573 <wkddpfla3@gmail.com>
2026-06-26 17:30:43 +0900

    init

---
+++ run/train_Mtransformers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#SBATCH -J SeCo_MTRANS_P1_V1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=29G
+#SBATCH -p batch_grad
+#SBATCH -w ariel-v2
+#SBATCH -t 4-0
+#SBATCH -o /nas2/data/dpfla3573/code/MM_SeCo/logs/slurm-%A_MTRANS_P1_V1.out
+
+cd /nas2/data/dpfla3573/code/MM_SeCo
+export PYTHONPATH=/nas2/data/dpfla3573/code/MM_SeCo:$PYTHONPATH
+
+/nas2/data/dpfla3573/anaconda3/envs/momask/bin/python run/train_t2m_transformer.py \
+  --name MTRANS_P1_V1\
+  --gpu_id 0 \
+  --dataset_name t2m \
+  --batch_size 64 \
+  --vq_name rvq_nq6_dc512_nc512_noshare_qdp0.2 \
