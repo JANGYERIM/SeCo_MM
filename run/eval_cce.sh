a88f25c3e0df63692b9434cb8d95a808570262eb
dpfla3573 <wkddpfla3@gmail.com>
2026-07-21 15:03:57 +0900

    P4

--- run/eval_cce.sh
+++ run/eval_cce.sh
@@ -5,17 +5,18 @@
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v2
+#SBATCH -w ariel-v4
 #SBATCH -t 1-0
-#SBATCH -o /nas2/data/dpfla3573/code/MM_SeCo/logs/slurm-%A_Eval_CCE_M_P2_V1_4.out
+#SBATCH -o /nas2/data/dpfla3573/code/MM_SeCo/logs/slurm-%A_Eval_CCE_PRETRAIN(temp=0.).out
 
 cd /nas2/data/dpfla3573/code/MM_SeCo
 export PYTHONPATH=/nas2/data/dpfla3573/code/MM_SeCo:$PYTHONPATH
 
 /nas2/data/dpfla3573/anaconda3/envs/momask/bin/python run/eval_cce.py \
-  --name M_P2_V1_4 \
+  --name t2m_nlayer8_nhead6_ld384_ff1024_cdp0.1_rvq6ns \
   --gpu_id 0 \
   --dataset_name t2m \
   --which_epoch all \
   --time_steps 10 \
+  --temperature 1e-6 \
   --res_name tres_nlayer8_ld384_ff1024_rvq6ns_cdp0.2_sw
